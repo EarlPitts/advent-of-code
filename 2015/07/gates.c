b24e02ee42b7710b0371d1dacdb9d5492ed54b05
Bendeguz Seres <k2sy12@inf.elte.hu>
2020-04-24 15:46:11 +0200

    Autocommit.

--- 2015/07/gates.c
+++ 2015/07/gates.c
@@ -8,10 +8,11 @@
 #define LSHIFT 4
 #define RSHIFT 5
 
-struct wire {
-    char* id;
-    int value;
-};
+typedef struct {
+    char** key;
+    int* value;
+    int cnt;
+} map;
 
 typedef struct {
     int gate;
@@ -19,6 +20,25 @@ typedef struct {
     char* in2;
     char* out; 
 } instruction;
+
+int get(map* m, char* key) {
+    int i = 0;
+
+    while (i < m->cnt) {
+        if (strcmp(m->key[i], key) == 0) return m->value[i];
+    }
+    
+    return -1;
+}
+
+void set(map* m, char* key, int value) {
+    m->cnt++;
+    m->key = realloc(m->key, sizeof(char*) * m->cnt);
+    m->key[m->cnt-1] = malloc(strlen(key) + 1);
+    strcpy(m->key[m->cnt-1], key);
+    m->value = realloc(m->value, sizeof(int*) * m->cnt);
+    m->value[m->cnt-1] = value;
+}
 
 void make_instr(char* line, int cnt, instruction* instructions) {
     instruction i;
@@ -34,14 +54,23 @@ void make_instr(char* line, int cnt, instruction* instructions) {
         else if (strcmp(next, "LSHIFT") == 0) i.gate = LSHIFT;
         else if (strcmp(next, "RSHIFT") == 0) i.gate = RSHIFT;
         else {
-            if (i.in1 == NULL) i.in1 = next;
-            else i.in2 = next;
+            if (i.in1 == NULL) {
+                i.in1 = malloc(strlen(next) + 1);
+                strcpy(i.in1, next);
+            }
+            else {
+                i.in2 = malloc(strlen(next) + 1);
+                strcpy(i.in2, next);
+            }
         }
 
         next = strtok(NULL, " ");
     }
 
-    i.out = strtok(NULL, "\n");
+    next = strtok(NULL, "\n");
+
+    i.out = malloc(strlen(next) + 1);
+    i.out = strcpy(i.out, next);
 
     instructions[cnt] = i;
 }
@@ -60,7 +89,13 @@ int main() {
         cnt++;
     }
 
+    map* m;
+    m = malloc(sizeof(map));
+    m->key = malloc(sizeof(char*));
+
+    m->cnt = 0;
     
 
+    
     return 0;
 }
