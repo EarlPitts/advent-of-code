39dd71acc480012d0d01d8bfea7ae4ff4d7c6194
Bendeguz Seres <grafitpor@gmail.com>
2023-08-24 10:04:24 +0200

    First part

---
+++ 2017/Day12.hs
@@ -0,0 +1,28 @@
+import Data.Graph
+import Text.Parsec
+import Text.Parsec.String
+import Data.Either
+
+type Node = Int
+
+pLine :: Parser (String, Node, [Node])
+pLine = do
+  n <- many1 digit
+  space
+  string "<->"
+  space
+  ns <- (fmap . fmap) read (sepBy (many1 digit) (string ", "))
+  newline
+  return (n,read n,ns)
+
+p :: Parser Graph
+p = do
+  lines <- many pLine
+  let (graph,_,_) = graphFromEdges lines
+  return graph
+
+main :: IO ()
+main = do
+  input <- getContents
+  let graph = fromRight (error "Oh no!") (parse p "" input)
+  print (length $ reachable graph 0)
