67e4842284709d6c90829923ec2f683647bb6432
Bendeguz Seres <grafitpor@gmail.com>
2023-10-29 15:21:33 +0100

    Solve part 2

--- 2016/03.hs
+++ 2016/03.hs
@@ -7,7 +7,16 @@ parse = fmap f . lines
 validate :: (Int,Int,Int) -> Bool
 validate (x,y,z) = x + y > z && x + z > y && y + z > x
 
+groupByNum :: Int -> [a] -> [[a]]
+groupByNum _ [] = []
+groupByNum n xs = take n xs : groupByNum n (drop n xs)
+
+tuplify :: [Int] -> (Int,Int,Int)
+tuplify [a,b,c] = (a,b,c)
+
 main :: IO ()
 main = do
   input <- parse <$> readFile "input"
   print $ length (filter validate input)
+  let (a,b,c) = unzip3 input
+  print $ length (filter validate (tuplify <$> groupByNum 3 (a ++ b ++ c)))
