424a8b95eaaf1cd9ab87c6e07e43bfae7cd6fcbf
Bendeguz Seres <grafitpor@gmail.com>
2024-02-04 07:50:55 +0100

    Solve part 2

--- 2018/src/main/scala/Day02.scala
+++ 2018/src/main/scala/Day02.scala
@@ -23,8 +23,18 @@ def hasN[A : Eq : Order](n: Int)(l: List[A]): Boolean =
 def checksum(l: List[String]): Int =
   l.map(_.toList).filter(hasN(2)).size * l.map(_.toList).filter(hasN(3)).size
 
+def differByOne[A: Eq](l1: List[A], l2: List[A]): Boolean =
+  l1.zip(l2).map((a,b) => (a == b)).filter(_ == false).size == 1
+
+def cross[A](l1: List[A], l2: List[A]): List[(A,A)] = l1.flatMap(a => l2.flatMap(b => List((a,b)))).distinct
+
+def solve(l: List[String]): String =
+  cross(l.map(_.toList),l.map(_.toList)).filter((a,b) => differByOne(a,b)).head match
+    case (l1,l2) => l1.zip(l2).filter((a,b) => a == b).map(_._1).mkString
+
 object Main extends IOApp.Simple:
   def run: IO[Unit] = for
     data <- getInput
     _ <- IO.println(checksum(data))
+    _ <- IO.println(solve(data))
   yield ()
