badfc77255acd59d7ad8b2099212b91ec2a715f6
Bendeguz Seres <grafitpor@gmail.com>
2025-11-23 08:12:30 +0100

    Add solution

--- 2023/Day15.hs
+++ 2023/Day15.hs
@@ -1,11 +1,19 @@
 module Day15 where
 
+import AoC.Utils
 import Control.Monad.State
 import Data.Char
+import qualified Data.List as L
 import Data.List.Split
-import AoC.Utils
+import qualified Data.Map as M
 
-input = "rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7"
+type Label = String
+
+type Length = Int
+
+data Step = Put Label Length | Remove Label deriving (Show, Eq)
+
+type HashMap = M.Map Int [(Label, Length)]
 
 hash :: String -> Int
 hash = foldl hashSingle 0
@@ -16,6 +24,42 @@ hashSingle curr c = mod ((curr + ord c) * 17) 256
 solution :: String -> Int
 solution = sum . (fmap hash) . splitOn ","
 
+parse :: String -> [Step]
+parse = fmap parseStep . splitOn ","
+
+parseStep :: String -> Step
+parseStep str = if op == '=' then Put label len else Remove label
+  where
+    label = takeWhile (\c -> not (c == '=' || c == '-')) str
+    op = head $ drop (length label) str
+    len = read $ drop ((length label) + 1) str
+
+hashMap :: [Step] -> HashMap
+hashMap = foldl f M.empty
+  where
+    f m (Put label len) = case M.lookup ind m of
+      Nothing -> M.insert ind [(label, len)] m
+      Just box -> case L.find (\(label', _) -> label' == label) box of
+        Nothing -> M.insert ind (box ++ [(label, len)]) m
+        Just _ -> M.insert ind (fmap (\orig@(label', _) -> if label' == label then (label, len) else orig) box) m
+      where
+        ind = hash label
+    f m (Remove label) = case M.lookup ind m of
+      Nothing -> m
+      Just box -> M.insert ind (L.filter (\(l, _) -> l /= label) box) m
+      where
+        ind = hash label
+
+power :: Int -> [(Label, Length)] -> Int
+power box ls = sum $ f <$> (zip [1 ..] lengths)
+  where
+    lengths = snd <$> ls
+    f (ind, len) = (box + 1) * ind * len
+
+solution' :: [Step] -> Int
+solution' steps = sum $ uncurry power <$> M.toList (hashMap steps)
+
 main = do
-  input <- readInput
-  print $ solution input
+  input <- (filter (not . (==) '\n')) <$> readInput
+  let steps = parse input
+  print $ solution' steps
